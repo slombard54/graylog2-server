fdc1639f7ffacc4eef8980d15cd641e00f801b3f
Dennis Oelkers <dennis@torch.sh>
2014-06-03 14:32:25 +0200

    Using MetricAsyncEventBus now, adding tunables for executor dimensions

--- graylog2-shared/src/main/java/org/graylog2/shared/internalevents/InternalEventHandlerImpl.java
+++ graylog2-shared/src/main/java/org/graylog2/shared/internalevents/InternalEventHandlerImpl.java
@@ -1,7 +1,9 @@
 package org.graylog2.shared.internalevents;
 
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
+import org.graylog2.shared.BaseConfiguration;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,13 +14,18 @@ import java.util.concurrent.*;
  */
 @Singleton
 public class InternalEventHandlerImpl implements InternalEventHandler {
-    private final AsyncEventBus eventBus;
+    private final EventBus eventBus;
 
     @Inject
-    public InternalEventHandlerImpl() {
-        BlockingQueue<Runnable> waitQueue = new ArrayBlockingQueue<Runnable>(1000);
-        Executor executor = new ThreadPoolExecutor(20, 100, 60, TimeUnit.SECONDS, waitQueue);
-        this.eventBus = new AsyncEventBus(executor);
+    public InternalEventHandlerImpl(BaseConfiguration configuration, MetricRegistry metricRegistry) {
+        BlockingQueue<Runnable> waitQueue = new ArrayBlockingQueue<Runnable>(configuration.getEventBusMaxQueueSize());
+
+        Executor executor = new ThreadPoolExecutor(configuration.getEventBusInitialPoolSize(),
+                configuration.getEventBusMaxPoolSize(),
+                configuration.getEventBusPoolKeepalive(), TimeUnit.SECONDS,
+                waitQueue);
+
+        this.eventBus = new MetricAsyncEventBus(executor, metricRegistry);
     }
 
     @Override
